f1200a57fcc9562fbf158586bc0187e92ee6303b
John Crisostomo <johncrisostomo@protonmail.ch>
2018-05-31 11:04:58 +0800

    Fix naming

--- Controllers/PropertiesController.cs
+++ Controllers/PropertiesController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace RESTDemo.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class PropertiesController : Controller
     {
         // GET api/values
         [HttpGet]
