a9336fe9b0d8a92201e9d213a94c74f394ec0147
John Crisostomo <johncrisostomo@protonmail.ch>
2018-05-31 11:08:21 +0800

    Corrects namespace

--- Controllers/PropertiesController.cs
+++ Controllers/PropertiesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace RESTDemo.Controllers
+namespace RESTDemo.API.Controllers
 {
     [Route("api/[controller]")]
     public class PropertiesController : Controller
--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace RESTDemo
+namespace RESTDemo.API
 {
     public class Program
     {
