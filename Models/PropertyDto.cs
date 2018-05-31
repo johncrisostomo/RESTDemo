3481173a73129c0cd71fc28e1196f049959ad3fd
John Crisostomo <johncrisostomo@protonmail.ch>
2018-05-31 11:17:08 +0800

    Fix wrong casing

--- Models/PropertyDto.cs
+++ Models/PropertyDto.cs
@@ -4,7 +4,7 @@ namespace RESTDemo.API.Models
     {
         public int Id { get; set; }
 
-        public string name { get; set; }
+        public string Name { get; set; }
 
         public string Description { get; set; }
     }
