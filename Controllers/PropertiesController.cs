9fb0d49b67fa908381715cf31b77fb5df373d364
John Crisostomo <johncrisostomo@protonmail.ch>
2018-05-31 11:19:15 +0800

    Implements GET

--- Controllers/PropertiesController.cs
+++ Controllers/PropertiesController.cs
@@ -6,36 +6,31 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace RESTDemo.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/properties")]
     public class PropertiesController : Controller
     {
-        // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            return Ok(PropertiesDataStore.Current.Properties);
         }
 
-        // GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
         [HttpPost]
         public void Post([FromBody]string value)
         {
         }
 
-        // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
