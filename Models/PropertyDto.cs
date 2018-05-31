da706023c7cc9f2705b3a053d3b240ce9d9b3252
John Crisostomo <johncrisostomo@protonmail.ch>
2018-05-31 11:10:06 +0800

    Creates PropertyDto as Mock model

---
+++ Models/PropertyDto.cs
@@ -0,0 +1,11 @@
+namespace RESTDemo.API.Models
+{
+    public class PropertyDto
+    {
+        public int Id { get; set; }
+
+        public string name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
